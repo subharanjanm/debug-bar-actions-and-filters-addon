13218ae6a4b2f7b748b78a4f49adeb6a4cd6d3f2
subharanjan <subharanjan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-03 09:29:18 +0000

    * Fixed HTML Validation error: "Saw U+0000 in stream." - props [Jrf](http://profiles.wordpress.org/jrf) * Moved css to separate file - props [Jrf](http://profiles.wordpress.org/jrf)
    git-svn-id: http://plugins.svn.wordpress.org/debug-bar-actions-and-filters-addon/trunk@814224 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class-debug-bar-action-and-filters-addon.php
+++ class-debug-bar-action-and-filters-addon.php
@@ -3,13 +3,17 @@
  * This file contains the classes those create two new tabs in the debug panel provided by Debug Bar plugin.
  * It extends the functionality provided by the parent plugin "Debug Bar".
  */
-
-class Debug_Bar_Actions_Addon_Panel extends Debug_Bar_Panel {
+class Debug_Bar_Actions_Filters_Addon extends Debug_Bar_Panel {
     private $tab_name;
     private $tab;
     private $callback;
     public function init() {
         $this->title( $this->tab );
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+    }
+    public function enqueue_scripts() {
+        wp_enqueue_style( 'debug-bar-actions-filters', plugins_url( 'css/debug-bar-actions-filters.css', __FILE__ ), array( 'debug-bar' ), false );
     }
     public function set_tab( $name, $callback ) {
         $this->tab_name = strtolower( preg_replace( "#[^a-z0-9]#msiU", "", $name ) );
@@ -24,24 +28,7 @@ class Debug_Bar_Actions_Addon_Panel extends Debug_Bar_Panel {
         echo call_user_func( $this->callback );
     }
 }
-
-class Debug_Bar_Filters_Addon_Panel extends Debug_Bar_Panel {
-    private $tab_name;
-    private $tab;
-    private $callback;
-    public function init() {
-        $this->title( $this->tab );
-    }
-    public function set_tab( $name, $callback ) {
-        $this->tab_name = strtolower( preg_replace( "#[^a-z0-9]#msiU", "", $name ) );
-        $this->tab = $name;
-        $this->callback = $callback;
-        $this->title( $this->tab );
-    }
-    public function prerender() {
-        $this->set_visible( true );
-    }
-    public function render() {
-        echo call_user_func( $this->callback );
-    }
+class Debug_Bar_Actions_Addon_Panel extends Debug_Bar_Actions_Filters_Addon {
+}
+class Debug_Bar_Filters_Addon_Panel extends Debug_Bar_Actions_Filters_Addon {
 }
--- debug-bar-action-and-filters-addon.php
+++ debug-bar-action-and-filters-addon.php
@@ -3,7 +3,7 @@
 Plugin Name: Debug Bar Actions and Filters Addon
 Plugin URI: http://wordpress.org/extend/plugins/debug-bar-actions-and-filters-addon/
 Description: This plugin add two more tabs in the Debug Bar to display hooks(Actions and Filters) attached to the current request. Actions tab displays the actions hooked to current request. Filters tab displays the filter tags along with the functions attached to it with priority.
-Version: 0.2
+Version: 0.3
 Author: Subharanjan
 Author Email: subharanjanmantri@gmail.com
 Author URI: http://www.subharanjan.in/
@@ -19,12 +19,12 @@ License: GPLv2
  */
 if ( !function_exists( 'debug_bar_action_and_filters_addon_panel' ) ) {
     function debug_bar_action_and_filters_addon_panel( $panels ) {
-        require_once( "class-debug-bar-action-and-filters-addon.php" );
+        require_once( plugin_dir_path( __FILE__ ) . 'class-debug-bar-action-and-filters-addon.php' );
         $wp_actions = new Debug_Bar_Actions_Addon_Panel();
-        $wp_actions->set_tab( "Action Hooks", "debug_bar_action_and_filters_addon_display_actions" );
+        $wp_actions->set_tab( 'Action Hooks', 'debug_bar_action_and_filters_addon_display_actions' );
         $panels[] = $wp_actions;
         $wp_filters = new Debug_Bar_Filters_Addon_Panel();
-        $wp_filters->set_tab( "Filter Hooks", "debug_bar_action_and_filters_addon_display_filters" );
+        $wp_filters->set_tab( 'Filter Hooks', 'debug_bar_action_and_filters_addon_display_filters' );
         $panels[] = $wp_filters;
         return $panels;
     }
@@ -34,20 +34,20 @@ add_filter( 'debug_bar_panels', 'debug_bar_action_and_filters_addon_panel' );
 /**
  * Function to display the Actions attached to current request.
  *
- * @return string $outout display output for the actions panel
+ * @return string $output display output for the actions panel
  */
 function debug_bar_action_and_filters_addon_display_actions() {
     global $wp_actions;
-    $output = "";
-    $output .= "<div class='hooks_listing_container'>";
-    $output .= "<h2 style='width:100%;'>List of Action Hooks</h2><br />";
-    $output .= "<ul style='list-style-type: square !important; padding-left: 20px !important;'>";
+    $output = '';
+    $output .= '<div class="hooks_listing_container">' . "\n";
+    $output .= '<h2>List of Action Hooks</h2><br />' . "\n";
+    $output .= "<ul>\n";
     foreach ( $wp_actions as $action_key => $action_val ) {
-        $output .= "<li>" . $action_key . "</li>";
+        $output .= '<li>' . $action_key . "</li>\n";
     }
-    $output .= "<li><strong>Total Count: </strong>" . count( $wp_actions ) . "</li>";
-    $output .= "</ul>";
-    $output .= "</div>";
+    $output .= '<li><strong>Total Count: </strong>' . count( $wp_actions ) . "</li>\n";
+    $output .= "</ul>\n";
+    $output .= "</div>\n";
     return $output;
 }
 
@@ -68,23 +68,23 @@ function isClosure( $arg ) {
 /**
  * Function to display the Filters applied to current request.
  *
- * @return string $outout display output for the filters panel
+ * @return string $output display output for the filters panel
  */
 function debug_bar_action_and_filters_addon_display_filters() {
     global $wp_filter;
-    $output = "";
-    $output .= "<div class='hooks_listing_container'>";
-    $output .= "<h2 style='width:100%;'>List of Filter Hooks (with functions)</h2><br />";
-    $output .= "<ul style='list-style-type: square !important; padding-left: 20px !important;'>";
+    $output = '';
+    $output .= '<div class="hooks_listing_container">' . "\n";
+    $output .= '<h2>List of Filter Hooks (with functions)</h2><br />' . "\n";
+    $output .= "<ul>\n";
     foreach ( $wp_filter as $filter_key => $filter_val ) {
-        $output .= "<li>";
-        $output .= "<strong>" . $filter_key . "</strong><br />";
-        $output .= "<ul style='list-style-type: square !important; padding-left: 20px !important;'>";
+        $output .= '<li>';
+        $output .= '<strong>' . $filter_key . "</strong><br />\n";
+        $output .= "<ul>\n";
         ksort( $filter_val );
         foreach ( $filter_val as $priority => $functions ) {
-            $output .= "<li>";
-            $output .= "Priority: " . $priority . "<br />";
-            $output .= "<ul style='list-style-type: square !important; padding-left: 20px !important;'>";
+            $output .= '<li>';
+            $output .= 'Priority: ' . $priority . "<br />\n";
+            $output .= "<ul>\n";
             foreach ( $functions as $single_function ) {
                 if ( !is_string( $single_function['function'] ) && ( !is_object( $single_function['function'] ) ) )
                     continue;
@@ -93,21 +93,21 @@ function debug_bar_action_and_filters_addon_display_filters() {
                 elseif ( ( isClosure( $single_function['function'][0] ) ) )
                     continue;
                 elseif ( is_string( $single_function['function'] ) )
-                    $output .= "<li>" . $single_function['function'] . "</li>";
+                    $output .= '<li>' . sanitize_text_field( $single_function['function'] ) . '</li>';
                 elseif ( is_array( $single_function['function'] ) && is_string( $single_function['function'][0] ) )
-                    $output .= "<li>" . $single_function['function'][0] . " -> " . $single_function['function'][1] . "</li>";
+                    $output .= '<li>' . sanitize_text_field( $single_function['function'][0] ) . ' -> ' . sanitize_text_field( $single_function['function'][1] ) . '</li>';
                 elseif ( is_array( $single_function['function'] ) && is_object( $single_function['function'][0] ) )
-                    $output .= "<li>(object) " . get_class( $single_function['function'][0] ) . " -> " . $single_function['function'][1] . "</li>";
+                    $output .= '<li>(object) ' . get_class( $single_function['function'][0] ) . ' -> ' . sanitize_text_field( $single_function['function'][1] ) . '</li>';
                 else
-                    $output .= "<li><pre>" . var_export( $single_function ) . "</pre></li>";
+                    $output .= '<li><pre>' . var_export( $single_function ) . '</pre></li>';
             }
-            $output .= "</ul>";
-            $output .= "</li>";
+            $output .= "</ul>\n";
+            $output .= "</li>\n";
         }
-        $output .= "</ul>";
-        $output .= "</li>";
+        $output .= "</ul>\n";
+        $output .= "</li>\n";
     }
-    $output .= "</ul>";
-    $output .= "</div>";
+    $output .= "</ul>\n";
+    $output .= "</div>\n";
     return $output;
 }
