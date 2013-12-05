20f1a0aab0506012c0633187d139d555a02ad1f9
jrfnl <github_nospam@adviesenzo.nl>
2013-12-05 14:41:26 +0100

    Small bits and pieces

--- debug-bar-action-and-filters-addon.php
+++ debug-bar-action-and-filters-addon.php
@@ -3,13 +3,13 @@
 	Plugin Name: Debug Bar Actions and Filters Addon
 	Plugin URI: http://wordpress.org/extend/plugins/debug-bar-actions-and-filters-addon/
 	Description: This plugin add two more tabs in the Debug Bar to display hooks(Actions and Filters) attached to the current request. Actions tab displays the actions hooked to current request. Filters tab displays the filter tags along with the functions attached to it with priority.
-	Version: 0.12
+	Version: 0.3
 	Author: Subharanjan
 	Author Email: subharanjanmantri@gmail.com
 	Author URI: http://www.subharanjan.in/
 	License: GPLv2
 	*/
-
+	
 	/**
 	 * Function to hook with debug_bar_panels filter.
 	 *
@@ -19,80 +19,78 @@
 	 */
 	if ( ! function_exists( 'debug_bar_action_and_filters_addon_panel' ) ) {
 		function debug_bar_action_and_filters_addon_panel( $panels ) {
-
 			require_once( plugin_dir_path( __FILE__ ) . 'class-debug-bar-action-and-filters-addon.php' );
-
 			$wp_actions = new Debug_Bar_Actions_Addon_Panel();
 			$wp_actions->set_tab( 'Action Hooks', 'debug_bar_action_and_filters_addon_display_actions' );
 			$panels[] = $wp_actions;
-
 			$wp_filters = new Debug_Bar_Filters_Addon_Panel();
 			$wp_filters->set_tab( 'Filter Hooks', 'debug_bar_action_and_filters_addon_display_filters' );
 			$panels[] = $wp_filters;
-
 			return $panels;
 		}
 	}
 	add_filter( 'debug_bar_panels', 'debug_bar_action_and_filters_addon_panel' );
-
+	
 	/**
 	 * Function to display the Actions attached to current request.
+	 *
 	 * @return string $output display output for the actions panel
 	 */
 	function debug_bar_action_and_filters_addon_display_actions() {
 		global $wp_actions;
-
 		$output = '';
 		$output .= '<div class="hooks_listing_container">' . "\n";
 		$output .= '<h2>List of Action Hooks</h2><br />'  . "\n";
 		$output .= "<ul>\n";
-	
-	
 		foreach ( $wp_actions as $action_key => $action_val ) {
 			$output .= '<li>' . $action_key . "</li>\n";
 		}
 		$output .= '<li><strong>Total Count: </strong>' . count( $wp_actions ) . "</li>\n";
 		$output .= "</ul>\n";
 		$output .= "</div>\n";
-
 		return $output;
 	}
-
-	# via http://stackoverflow.com/questions/4127959/testing-for-a-php-closure-without-referring-to-the-closure-internal-class
+	
+	/**
+	 * Function to to check for clousures
+	 *
+	 * @param   mixed $arg function name
+	 *
+	 * @return  boolean $closurecheck return whether or not a closure
+	 */
 	function isClosure( $arg ) {
-	    $test = function(){};
-	    return $arg instanceof $test;
+	    $test = function () {
+	    };
+	    $closurecheck = ( $arg instanceof $test );
+	    return $closurecheck;
 	}
-
+	
 	/**
 	 * Function to display the Filters applied to current request.
+	 *
 	 * @return string $output display output for the filters panel
 	 */
 	function debug_bar_action_and_filters_addon_display_filters() {
 		global $wp_filter;
-
 		$output = '';
 		$output .= '<div class="hooks_listing_container">' . "\n";
 		$output .= '<h2>List of Filter Hooks (with functions)</h2><br />' . "\n";
 		$output .= "<ul>\n";
-	
 		foreach ( $wp_filter as $filter_key => $filter_val ) {
 			$output .= '<li>';
 			$output .= '<strong>' . $filter_key . "</strong><br />\n";
 			$output .= "<ul>\n";
 			ksort( $filter_val );
 			foreach ( $filter_val as $priority => $functions ) {
-
 				$output .= '<li>';
 				$output .= 'Priority: ' . $priority . "<br />\n";
-
 				$output .= "<ul>\n";
 				foreach ( $functions as $single_function ) {
 					if ( !is_string( $single_function['function'] ) && ( !is_object( $single_function['function'] ) ) )
 						continue;
-					elseif ( ( isClosure($single_function['function'] ) ) )
+	                elseif ( ( isClosure( $single_function['function'] ) ) )
 						continue;
-					elseif ( ( isClosure($single_function['function'][0] ) ) )
+	                elseif ( ( isClosure( $single_function['function'][0] ) ) )
 						continue;						
 					elseif ( is_string( $single_function['function'] ) )
 						$output .= '<li>' . sanitize_text_field( $single_function['function'] ) . '</li>';
@@ -102,19 +100,14 @@
 						$output .= '<li>(object) ' . get_class( $single_function['function'][0] ) . ' -> ' . sanitize_text_field( $single_function['function'][1] ) . '</li>';
 					else
 						$output .= '<li><pre>' . var_export( $single_function ) . '</pre></li>';
-
 				}
 				$output .= "</ul>\n";
-
 				$output .= "</li>\n";
-
 			}
 			$output .= "</ul>\n";
 			$output .= "</li>\n";
-
 		}
 		$output .= "</ul>\n";
 		$output .= "</div>\n";
-
 		return $output;
 	}
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Contributors: subharanjan
 Tags: Debug Bar, Actions, Filters, Debug Bar Actions Display, Debug Bar Filters Display, List Hooks attached, List of Hooks Fired, Developer's tool for action and filter hooks
 Requires at least: 3.2
 Tested up to: 3.7.1
-Stable tag: trunk
+Stable tag: 0.3
 License: GPLv2
 
 This plugin adds two more tabs in the Debug Bar to display all the hooks(Actions and Filters) for the current request. Requires "Debug Bar" plugin.
@@ -13,13 +13,17 @@ This plugin adds two more tabs in the Debug Bar to display all the hooks(Actions
 
 This plugin adds two more tabs in the Debug Bar to display hooks(Actions and Filters) attached to the current request. Actions tab displays the actions hooked to current request. Filters tab displays the filter tags along with the functions attached to it with respective priority.  
   
-**Note:** [Debug Bar](http://wordpress.org/extend/plugins/debug-bar/) plugin must be installed prior to this.   
+**Note:**
+Debug Bar plugin must be installed prior to this.
+( http://wordpress.org/extend/plugins/debug-bar/ )
 
 == Installation ==
 
 Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.   
   
-**Note:** [Debug Bar](http://wordpress.org/extend/plugins/debug-bar/) plugin must be installed prior to this.   
+**Note:**
+Debug Bar plugin must be installed prior to this.
+( http://wordpress.org/extend/plugins/debug-bar/ )
 
 Don't use this on Live site. This is only for development purpose.
 
@@ -30,18 +34,28 @@ Don't use this on Live site. This is only for development purpose.
 
 == Frequently Asked Questions ==
 1. Can it be used on live site ?
-And: Please don't use this on Live site. This is only for development purpose.
+And: Please don't use this on live site. This is only for development purpose.
 
 == Changelog ==
 
-= 0.12 =
+= 0.3 =
 * Fixed HTML Validation error: "Saw U+0000 in stream." - props [Jrf](http://profiles.wordpress.org/jrf)
 * Moved css to separate file - props [Jrf](http://profiles.wordpress.org/jrf)
 
-= 0.11 -
+= 0.2 =
+* Fix for a closure issue.
+
+= 0.11 =
+* Fix for a fatal error bacuase of BuddyPress hooks
+* Closed the ul tag.
+
+= 0.1 =
 Adding the initial plugin to Wordpress plugins directory.
 
 == Upgrade Notice ==
 
-= 0.12 =
+= 0.3 =
 Some bug fixes
+
+= 0.1 =
+New Installation
